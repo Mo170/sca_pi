623290f643d32982a64b0c3628f11622a2a31608
Mo170 <mchar1703@gmail.com>
2018-06-05 15:46:03 -0500

    more scripts

---
+++ input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "what are your 3 favorite fruits?"
+read fruit1 fruit2 fruit3
+
+echo Your first was $fruit1 
+echo Your second fruit was $fruit2
+echo Your third fruit was fruit $fruit3
